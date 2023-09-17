35e7a3d84abbc1d8a0066fa52b538fe6d66812c4
joaoreis28 <reisjoaomendes@gmail.com>
2023-09-17 13:36:32 -0300

    Updating.

--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "galeria.apps.GaleriaConfig", #nossos apps
+    "usuarios.apps.UsuariosConfig",
 ]
 
 MIDDLEWARE = [
