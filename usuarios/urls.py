62ed54ef16d98a6fb65079297518471686fb2856
joaoreis28 <reisjoaomendes@gmail.com>
2023-09-18 14:46:40 -0300

    Adding Forms.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from usuarios.views import login, cadastro
 
-urlspatterns = [
+urlpatterns = [
     path('login', login, name='login'),
-    path('cadastro', cadastro, name='cadastro')
+    path('cadastro', cadastro, name='cadastro'),
 ]
